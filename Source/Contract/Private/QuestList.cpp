3a960bd4092cbe314cb7fd1de25e6f71c9b07d36
RedZoo <hyunsim01@naver.com>
2025-05-08 09:25:24 +0900

    퀘스트 목록 구조 생성

---
+++ Source/Contract/Private/QuestList.cpp
@@ -0,0 +1,2 @@
+﻿#include "QuestList.h"
+
---
+++ Source/Contract/Public/QuestList.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "QuestList.generated.h"
+
+class AItem;
+
+USTRUCT(BlueprintType)
+struct FQuestInfo
+{
+	GENERATED_BODY()
+
+	// Quest name
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+	FString questName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+	FString content;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+	FString conditions;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+	TArray<AItem*> rewards;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+	int money;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+	int experiencePoints;
+};
+
+UCLASS()
+class CONTRACT_API UQuestList : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	// List of quests
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+	TArray<FQuestInfo> Quests;
+};
